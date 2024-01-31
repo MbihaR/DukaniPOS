c2bea4ed190184ce1dab09688f9606474e551bc1
mbiha <104499568+mbiha@users.noreply.github.com>
2024-01-31 21:57:33 +0300

    initial commit

---
+++ PointOfSales/Forms/frmReports.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PointOfSales.Forms
+{
+    partial class frmReports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdoInventory = new System.Windows.Forms.RadioButton();
+            this.rdoSales = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 31F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1007, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reports";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.rdoInventory);
+            this.panel1.Controls.Add(this.rdoSales);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 69);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(203, 502);
+            this.panel1.TabIndex = 1;
+            // 
+            // rdoInventory
+            // 
+            this.rdoInventory.AutoSize = true;
+            this.rdoInventory.Location = new System.Drawing.Point(33, 112);
+            this.rdoInventory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoInventory.Name = "rdoInventory";
+            this.rdoInventory.Size = new System.Drawing.Size(103, 20);
+            this.rdoInventory.TabIndex = 3;
+            this.rdoInventory.TabStop = true;
+            this.rdoInventory.Text = "radioButton1";
+            this.rdoInventory.UseVisualStyleBackColor = true;
+            // 
+            // rdoSales
+            // 
+            this.rdoSales.AutoSize = true;
+            this.rdoSales.Location = new System.Drawing.Point(33, 73);
+            this.rdoSales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoSales.Name = "rdoSales";
+            this.rdoSales.Size = new System.Drawing.Size(63, 20);
+            this.rdoSales.TabIndex = 3;
+            this.rdoSales.TabStop = true;
+            this.rdoSales.Text = "Sales";
+            this.rdoSales.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(9, 208);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(185, 126);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Date Filter";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "List of Products";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(204, 69);
+            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.SelectionFormula = "";
+            this.crystalReportViewer1.ShowCloseButton = false;
+            this.crystalReportViewer1.ShowGroupTreeButton = false;
+            this.crystalReportViewer1.ShowRefreshButton = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(802, 502);
+            this.crystalReportViewer1.TabIndex = 2;
+            this.crystalReportViewer1.ToolPanelWidth = 267;
+            this.crystalReportViewer1.ViewTimeSelectionFormula = "";
+            // 
+            // frmReports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 571);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmReports";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Point of Sales";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoInventory;
+        private System.Windows.Forms.RadioButton rdoSales;
+    }
+}
